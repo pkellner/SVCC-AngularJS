6b4ffb6c5d40696fc81a1c73fcd2d56b011a0419
pkellner <peter@peterkellner.net>
2014-11-16 17:56:53 -0800

    wip

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
