03068bd9f67f0462b2b75af212bc07f0b3da2705
pkellner <peter@peterkellner.net>
2014-11-16 21:06:35 -0800

    wi8p

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -11,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -19,10 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.use('/', routes);
 app.use('/users', users);
@@ -60,7 +58,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-var port = process.env.port;
-app.listen(port);
-console.log('Listening on ' + port);
