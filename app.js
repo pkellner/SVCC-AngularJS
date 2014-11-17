555cc0b7b8e07cc44b5d0ef2b33af8b88a6fc335
pkellner <peter@peterkellner.net>
2014-11-16 17:55:22 -0800

    wip

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-compass')({mode: 'expanded'}));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
