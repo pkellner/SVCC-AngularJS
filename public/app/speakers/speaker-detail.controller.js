e2b8c9721ff4232e4fdb4b579af3baf3e3bdd954
peter@peterkellner.net <peter@peterkellner.net>
2014-11-25 13:41:28 -0800

    wip

--- public/app/speakers/speaker-detail.controller.js
+++ public/app/speakers/speaker-detail.controller.js
@@ -8,17 +8,31 @@
 
         var vm = this;
         vm.speakers = [];
-        speakerResourceService.query(function (speaker) {
-            angular.forEach(speaker, function (value) {
-                if (String(value.id) === $routeParams.id) {
-                    vm.speakers.push(value);
-                }
-            });
-            if (vm.speakers.length > 0) {
-                vm.speaker = vm.speakers[0];
-            }
 
-        });
+        if ($routeParams.id) {
+
+            speakerResourceService.get({id: $routeParams.id},
+                function (data) {
+                    console.log('speakerResourceService success for one entry');
+                    vm.speaker = data;
+                });
+
+        }
+        else {
+            speakerResourceService.query(function (speaker) {
+                angular.forEach(speaker, function (value) {
+                    if (String(value.id) === $routeParams.id) {
+                        vm.speakers.push(value);
+                    }
+                });
+                if (vm.speakers.length > 0) {
+                    vm.speaker = vm.speakers[0];
+                }
+
+            });
+        }
+
+
     }
 
     SpeakerDetailController.$inject = ['$routeParams', 'speakerResourceService'];
--- public/app/speakers/speaker-resource.service.js
+++ public/app/speakers/speaker-resource.service.js
@@ -4,8 +4,7 @@
     angular.module('svccApp')
         .factory('speakerResourceService',['$resource',
         function ($resource) {
-            //return $resource("/rest/presenter?arrayOnly=true");
-            return $resource('data/speakers.json');
+            return $resource('/rest/presenter/arrayonly/:id');
+            //return $resource('data/speakers.json');
         }]);
-
 }());
