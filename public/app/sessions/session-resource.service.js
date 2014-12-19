91f7603d109165bb0f267ec6fc137cfdfdb7dd2a
pkellner <peter@peterkellner.net>
2014-12-19 14:31:00 -0800

    wip

--- public/app/sessions/session-resource.service.js
+++ public/app/sessions/session-resource.service.js
@@ -5,8 +5,8 @@
         factory('sessionResourceService', sessionResourceService);
 
     function sessionResourceService($resource) {
-        return $resource('/rest/session/arrayonly/:id');
-        //return $resource('data/sessions.json');
+        //return $resource('/rest/session/arrayonly/:id');
+        return $resource('data/sessions.json');
     }
 
     sessionResourceService.$inject = ['$resource'];
