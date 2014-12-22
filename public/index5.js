6ebfa05740e2a531f1aa62268a126e34f13c6f24
pkellner <peter@peterkellner.net>
2014-12-22 08:19:04 -0500

    wip

--- public/index5.js
+++ public/index5.js
@@ -6,39 +6,38 @@
         'ui.router'
     ]);
 
-
     app.config(['$stateProvider', '$urlRouterProvider',
 
         function ($stateProvider, $urlRouterProvider,$q,$timeout) {
             $urlRouterProvider.otherwise('/');
 
             $stateProvider
-                .state('home', {
+                .state('init', {
                     url: '/',
-                    template: '<p>HOME HERE</p>',
-                    controller: 'HomeController as vm'
+                    template: '<p>initialization</p>',
+                    controller: 'InitController as vm'
                 }).
-                state('about', {
+                state('ready', {
                     url: '/about',
-                    template:   '<p>about here title: {{vm.title}}</p>',  //'index4template.html',
-                    controller: 'AboutController as vm',
+                    template:   '<p>ready</p>',
+                    controller: 'ReadyController as vm',
 
                     resolve: {
-                        title: function(){
-                            return 'from title function';
+                        app: function ($q, $timeout) {
+                            var defer = $q.defer;
+                            // this will be http that gets init data
+                            $timeout(function () {
+                                console.log('timeout completed')
+                                defer.resolve();
+                            }, 3000);
+                            return defer.promise;
                         }
                     }
                 });
         }]);
 
 
-    var injectParamsAbout = ['title'];
-    var AboutController = function (title) {
-        var vm = this;
-        vm.title = title;
-    };
-    AboutController.$inject = injectParamsAbout;
-    angular.module('svccApp').controller('AboutController', AboutController);
+    /*-----------------------------------------*/
 
     var injectParamsHome = [];
     var HomeController = function () {
@@ -46,6 +45,46 @@
     HomeController.$inject = injectParamsHome;
     angular.module('svccApp').controller('HomeController', HomeController);
 
+
+    var injectParamsAbout = ['title'];
+    var AboutController = function (title) {
+    };
+    AboutController.$inject = injectParamsAbout;
+    angular.module('svccApp').controller('AboutController', AboutController);
+
+    /*-----------------------------------------*/
+
+    var injectParamsInit = [];
+    var InitController = function () {
+
+
+
+    };
+    HomeController.$inject = injectParamsInit;
+    angular.module('svccApp').controller('InitController', InitController);
+
+
+
+    var injectParamsReady = ['$urlRouter'];
+    var ReadyController = function ($stateProvider) {
+
+        debugger;
+        $stateProvider
+            .state('home', {
+                url: '/',
+                template: '<p>HOME HERE</p>',
+                controller: 'HomeController as vm'
+            }).
+            state('about', {
+                url: '/about',
+                template:   '<p>about controller</p>',  //'index4template.html',
+                controller: 'AboutController as vm'
+            });
+    };
+    ReadyController.$inject = injectParamsReady;
+    angular.module('svccApp').controller('ReadyController', ReadyController);
+
+    /*-----------------------------------------*/
 
 
 }());
