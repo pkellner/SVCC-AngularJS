f75c6c45447cb1aa0403f2a5bfa94315c537d416
pkellner <peter@peterkellner.net>
2014-12-26 09:54:56 -0800

    wip

--- public/app/svcc/speakers/speaker-datamodel.service.js
+++ public/app/svcc/speakers/speaker-datamodel.service.js
@@ -32,74 +32,7 @@
             return this.getData();
         };
 
-        //// options parameter is an object with key value pairs
-        //// in this simple implementation, value is limited to a single value (no arrays)
-        //this.findMany = function (options) {
-        //    // find games that match all of the options
-        //    var list = $.grep(this.getData(), function (element, index) {
-        //        var matchAll = true;
-        //        $.each(options, function (optionKey, optionValue) {
-        //            if (element[optionKey] != optionValue) {
-        //                matchAll = false;
-        //                return false;
-        //            }
-        //        });
-        //        return matchAll;
-        //    });
-        //};
 
-        //add a new data item that does not exist already
-        //must compute a new unique id and backfill in
-        //this.addOne = function (dataItem) {
-        //    // must calculate a unique ID to add the new data
-        //    var newId = this.newId();
-        //    dataItem.gameid = newId;
-        //    this.data.push(dataItem);
-        //    return dataItem;
-        //};
-
-        //// return an id to insert a new data item at
-        //this.newId = function () {
-        //    // find all current ids
-        //    var currentIds = $.map(this.getData(), function (dataItem) {
-        //        return dataItem.gameid;
-        //    });
-        //    // since id is numeric, and we will treat like an autoincrement field, find max
-        //    var maxId = Math.max.apply(Math, currentIds);
-        //    // increment by one
-        //    return maxId + 1;
-        //};
-
-        //this.updateOne = function (gameid, dataItem) {
-        //    // find the game that matches that id
-        //    var games = this.getData();
-        //    var match = null;
-        //    for (var i = 0; i < games.length; i++) {
-        //        if (games[i].gameid == gameid) {
-        //            match = games[i];
-        //            break;
-        //        }
-        //    }
-        //    if (!angular.isObject(match)) {
-        //        return {};
-        //    }
-        //    angular.extend(match, dataItem);
-        //    return match;
-        //};
-
-        //this.deleteOne = function (gameid) {
-        //    // find the game that matches that id
-        //    var games = this.getData();
-        //    var match = false;
-        //    for (var i = 0; i < games.length; i++) {
-        //        if (games[i].gameid == gameid) {
-        //            match = true;
-        //            games.splice(i, 1);
-        //            break;
-        //        }
-        //    }
-        //    return match;
-        //};
 
         this.data =  [
             {
--- public/app/svcc/speakers/speaker-resource.service.js
+++ public/app/svcc/speakers/speaker-resource.service.js
@@ -2,9 +2,13 @@
     'use strict';
 
     angular.module('svccApp')
-        .factory('speakerResourceService',['$resource',//'speakerResourceServiceMock',
-        function ($resource) {
-            return $resource('/rest/presenter/arrayonly/:id');
-            //return $resource('data/speakers.json');
-        }]);
+        .factory('speakerResourceService', ['$resource',//'speakerResourceServiceMock',
+            function ($resource) {
+
+                return $resource('/rest/presenter/arrayonly/:id', {}, {
+                    'get': {method: 'GET', cache: true},
+                    'query': {method: 'GET', cache: true, isArray: true}
+                });
+                //return $resource('data/speakers.json');
+            }]);
 }());
