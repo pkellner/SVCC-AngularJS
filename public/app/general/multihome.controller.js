be4e58a3d805fc1547281e85aad01e725eaa70ad
pkellner <peter@peterkellner.net>
2015-01-03 13:56:39 -0800

    wip

--- public/app/general/multihome.controller.js
+++ public/app/general/multihome.controller.js
@@ -4,35 +4,52 @@
     angular.module('baseApp')
         .controller('MultihomeController', MultihomeController);
 
-    function MultihomeController($state, accountInfoService, $rootScope, $timeout) {
+    function MultihomeController($state, accountInfoService, $rootScope, $timeout, CONFIG) {
 
-        accountInfoService.save().$promise.then(
-            function (data) {
-                if (data && data.codeCampType) {
-                    var codeCampType = data.codeCampType;
-                    $rootScope.configData = data;
-                    $timeout(function () {
-                            $rootScope.hideLoadingIcon = true;
-                            if (codeCampType === 'svcc') {
-                                $state.transitionTo('svcc.home');
-                            } else if (codeCampType === 'conf') {
-                                $state.transitionTo('conf.home');
-                            } else if (codeCampType === 'angu') {
-                                $state.transitionTo('angu.home');
-                            }
-                        }, 1000
-                    );
-                } else {
-                    console.log('data problem in MultihomeController');
+        var codeCampType = CONFIG.codeCampType;
+        debugger;
+
+        $timeout(function () {
+                $rootScope.hideLoadingIcon = true;
+                if (codeCampType === 'svcc') {
+                    $state.transitionTo('svcc.home');
+                } else if (codeCampType === 'conf') {
+                    $state.transitionTo('conf.home');
+                } else if (codeCampType === 'angu') {
+                    $state.transitionTo('angu.home');
                 }
-            },
-            function (error) {
-                console.log('MultihomeController error: ' + error);
-            });
+            }, 1000
+        );
+
+
+        //accountInfoService.save().$promise.then(
+        //    function (data) {
+        //        debugger;
+        //        if (data && data.codeCampType) {
+        //            var codeCampType = data.codeCampType;
+        //            $rootScope.configData = data;
+        //            $timeout(function () {
+        //                    $rootScope.hideLoadingIcon = true;
+        //                    if (codeCampType === 'svcc') {
+        //                        $state.transitionTo('svcc.home');
+        //                    } else if (codeCampType === 'conf') {
+        //                        $state.transitionTo('conf.home');
+        //                    } else if (codeCampType === 'angu') {
+        //                        $state.transitionTo('angu.home');
+        //                    }
+        //                }, 1000
+        //            );
+        //        } else {
+        //            console.log('data problem in MultihomeController');
+        //        }
+        //    },
+        //    function (error) {
+        //        console.log('MultihomeController error: ' + error);
+        //    });
 
     }
 
-    MultihomeController.$inject = ['$state', 'accountInfoService', '$rootScope', '$timeout'];
+    MultihomeController.$inject = ['$state', 'accountInfoService', '$rootScope', '$timeout', 'CONFIG'];
 
     angular.element(document).ready(function () {
 
