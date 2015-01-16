38500d5caa71c6e546a60e29495523af2919d2cc
thymikee <michal@xhtmlized.com>
2015-01-16 18:26:25 +0100

    Got browsersync to work properly. Started developing header part of the site

--- tasks/gwatch.js
+++ tasks/gwatch.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 
-gulp.task('gwatch', ['browser-sync'], function () {
+gulp.task('gwatch', ['sass', 'browser-sync'], function () {
     gulp.watch('assets/scss/**/*.scss', ['sass']);
+    gulp.watch('public/**/*.html', ['bs-reload']);
 });
--- tasks/sass.js
+++ tasks/sass.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var config = require('./config.json');
 var browserSync = require('browser-sync');
+var filter = require('gulp-filter');
 
 gulp.task('sass', function () {
   return gulp.src(config.styles.files)
@@ -11,11 +12,6 @@ gulp.task('sass', function () {
     .pipe($.sass(config.styles.sassOptions))
     .pipe($.autoprefixer(config.styles.AUTOPREFIXER_BROWSERS))
     .pipe(gulp.dest(config.styles.build))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-    .pipe($.notify({
-      title: 'SASS task',
-      message: 'Completed successfully!'
-    }));
+    .pipe(filter('**/*.css'))
+    .pipe(browserSync.reload({stream: true}));
 });
