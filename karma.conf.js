4af5c6ef9992e6fd6eb3893ef3e45eac93a0a88d
Ben Drucker <bvdrucker@gmail.com>
2015-02-06 10:34:39 -0500

    Load Angular using browserify-shim instead of expecting global

--- karma.conf.js
+++ karma.conf.js
@@ -2,8 +2,6 @@ module.exports = function (config) {
   config.set({
     frameworks: ['browserify', 'mocha'],
     files: [
-      './node_modules/angular/angular.js',
-      './node_modules/angular-mocks/angular-mocks.js',
       './test/unit/index.js'
     ],
     exclude: [
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var angular = require('angular');
+require('angular-mocks');
 
 describe('App', function () {
 
