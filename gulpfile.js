2a9f5d3f083ab395b00d9177d34ca5c2b31a1ae0
Ben Drucker <bvdrucker@gmail.com>
2015-02-07 18:05:28 -0500

    Migrate karma config into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,26 @@
 
 var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var karma   = require('karma-as-promised');
+
+gulp.task('unit', function () {
+  return karma.server.start({
+    frameworks: ['browserify', 'mocha'],
+    files: [
+      './test/unit/index.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      './test/unit/index.js': 'browserify'
+    },
+    browserify: {
+      transform: ['browserify-istanbul']
+    },
+    reporters: ['progress', 'coverage'],
+    port: 9876,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false
+  });
+});
