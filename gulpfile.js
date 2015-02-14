3fe01a2015bcd56d2595a60d507ac0446624c2df
Ben Drucker <bvdrucker@gmail.com>
2015-02-14 12:20:04 -0500

    fix template task (never interpolated path)

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ gulp.task('index', function () {
     .pipe(gulp.dest('dist'));
 });
 
-paths.templates = format('./%s/src/**/*.html');
+paths.templates = format('./%s/src/**/*.html', app);
 gulp.task('templates', function () {
   return gulp.src(paths.templates)
     .pipe(gulp.dest('dist/app'));
