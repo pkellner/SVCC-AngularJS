2a5eccd6e931d3b9247eab4e5f35fdc1ec3c4be4
Ben Drucker <bvdrucker@gmail.com>
2015-02-14 11:38:46 -0500

    Remove home state from base

--- base/src/static/states.js
+++ base/src/static/states.js
@@ -2,10 +2,6 @@
 
 exports = module.exports = function ($stateProvider, campTemplate) {
   $stateProvider
-    .state('base.home', {
-      url: '/',
-      templateProvider: campTemplate.provide('app/{{codeCampType}}/miscpages/{{codeCampType}}.html')
-    })
     .state('base.about', {
       url: '/about',
       templateProvider: campTemplate.provide('app/{{codeCampType}}/miscpages/about.html')
