ccc4a5db96cfc4983c9764605b10af79a1df5065
Ben Drucker <bvdrucker@gmail.com>
2015-02-22 17:10:24 -0500

    Rewrite speaker social icons directive

--- angu/src/speakers/index.js
+++ angu/src/speakers/index.js
@@ -2,8 +2,10 @@
 
 import angular from 'angular';
 import details from './details';
+import social from './social-icons';
 
 export default angular.module('angu.speakers', [
     details
   ])
+  .directive('speakerSocialIcons', social)
   .name;
