afb0037a50f1177dbaf6df9687e05a3b27af6e51
Ben Drucker <bvdrucker@gmail.com>
2015-03-05 18:22:51 -0500

    Filter sessions by selected day and track

--- angu/src/sessions/list/controller.js
+++ angu/src/sessions/list/controller.js
@@ -7,6 +7,18 @@ function SessionListController (sessions, days, tracks) {
   this.days = days;
   this.day = days[0];
   this.tracks = tracks;
+  this.track = undefined;
+
+  this.filter = (session) => {
+    if (this.day.dayOfWeek !== 'Show All') {
+      const dayIndex = new Date(session.sessionTimeDateTime).getDay();
+      if (!this.day.is(dayIndex)) return;
+    }
+    if (this.track && this.track.id !== session.sessionTrackId) {
+      return;
+    }
+    return true;
+  }
 }
 SessionListController.$inject = ['sessions', 'days', 'tracks'];
 
