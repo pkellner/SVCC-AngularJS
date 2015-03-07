d47166edde96b7ff99626dd44f0c4c6ab7305ba9
Ben Drucker <bvdrucker@gmail.com>
2015-03-07 14:36:43 -0500

    Redirect /speakers... to /speaker...

--- angu/src/speakers/index.js
+++ angu/src/speakers/index.js
@@ -12,4 +12,16 @@ export default angular.module('angu.speakers', [
   ])
   .directive('speakerSocialIcons', social)
   .directive('speakerBio', bio)
+  .config(redirect)
   .name;
+
+
+redirect.$inject = ['$urlRouterProvider']
+function redirect ($urlRouterProvider) {
+  $urlRouterProvider.when(/^\/speakers/, singularizeUrl);
+}
+
+singularizeUrl.$inject = ['$match'];
+function singularizeUrl ($match) {
+  return $match.input.replace('/speakers', '/speaker');
+}
