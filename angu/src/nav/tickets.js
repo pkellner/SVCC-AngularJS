83757e5be5d44f3ef9d566b814fc841662fb1089
thymikee <thymikee@gmail.com>
2015-03-11 10:55:51 +0100

    Display "Get Tickets" button and hotel w/ exapandable email input

--- angu/src/nav/tickets.js
+++ angu/src/nav/tickets.js
@@ -3,13 +3,18 @@
 exports = module.exports = function ($window, config) {
 
   function TicketNotificationController () {
+    this.requesting = false;
     this.email = null;
     this.username = config.user.active;
     this.hotelBookingUrl = config.hotel.bookingUrl;
-  };
+  }
 
   TicketNotificationController.prototype.request = function (email) {
     $window.location.href = '/angu/signuptobenotified?email=' + email;
+  };
+
+  TicketNotificationController.prototype.showForm = function () {
+    this.requesting = true;
   };
 
   return {
@@ -21,4 +26,4 @@ exports = module.exports = function ($window, config) {
   };
 
 };
-exports.$inject = ['$window', 'config']
+exports.$inject = ['$window', 'config'];
