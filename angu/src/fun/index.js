969e84485043d086b1cc23bfb8235a1aa7f9ab2e
thymikee <michal@xhtmlized.com>
2015-03-19 13:33:45 +0100

    Ugly fix for Angular Masonry's bug when not generating bricks with ng-repeat. Had to mock one

--- angu/src/fun/index.js
+++ angu/src/fun/index.js
@@ -4,8 +4,12 @@ import angular from 'angular';
 import masonry from 'angular-masonry';
 import state from './state';
 
+
 export default angular.module('angu.fun', [
   masonry
 ])
+.controller('TilesCtrl', ['$scope', function ($scope) {
+    $scope.bricks = [ null ];
+  }])
 .config(state)
 .name;
