cc21f0e9593f209ed2448c6b3247ebcfabc42f99
Ben Drucker <bvdrucker@gmail.com>
2015-03-18 21:48:09 -0400

    Install and configure masonry

--- angu/src/fun/index.js
+++ angu/src/fun/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 import angular from 'angular';
+import masonry from 'angular-masonry';
 import state from './state';
 
-export default angular.module('angu.fun', [])
-  .config(state)
-  .name;
+export default angular.module('angu.fun', [
+  masonry
+])
+.config(state)
+.name;
