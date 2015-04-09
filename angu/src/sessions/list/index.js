523c27bf5d9450a3b560d50899e7086c86dee772
Ben Drucker <bvdrucker@gmail.com>
2015-04-09 15:48:33 -0400

    Hide session time in program for repeated sessions w/ same time

--- angu/src/sessions/list/index.js
+++ angu/src/sessions/list/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 import angular from 'angular';
+import item from './item';
 import controller from './controller';
 import state from './state';
 
 export default angular.module('angu.sessions.list', [])
+  .directive('sessionListItem', item)
   .controller('SessionListController', controller)
   .config(state)
   .name;
