f3240808fea810089f65bee991586d06279e31cb
pkellner <peter@peterkellner.net>
2015-04-11 13:03:13 -0700

    Changed A-B Test to test words on "Buy" button. Later will refactor variable names to not reflect intent (that is, title2->miscString2 and titleGuid->miscGuid or something like that.

--- angu/src/nav/tickets.js
+++ angu/src/nav/tickets.js
@@ -5,14 +5,17 @@ import template from './tickets.html';
 exports = module.exports = function ($window, config) {
 
   function TicketNotificationController () {
-    this.requesting = false;
+    this.requesting = false;  // what does "requesting" do? I can't find usage.  4/11/2015 - PGK
+
     this.email = null;
     this.username = config.user.active;
     this.hotelBookingUrl = config.hotel.bookingUrl;
     this.purchaseUrl = '/angu/purchasetickets';
-
     const titleGuid = config.home.title2.guid;
-    if (titleGuid) this.purchaseUrl += `?value=${titleGuid}`;
+    if (titleGuid) {
+      this.purchaseUrl += `?value=${titleGuid}`;
+    }
+    this.title2 = config.home.title2.content ? config.home.title2.content :  'Buy Ticket';
   }
 
   TicketNotificationController.prototype.request = function (email) {
