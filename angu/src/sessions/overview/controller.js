8b805c5ea845ffa5b137f794469f0a34822b032a
pkellner <peter@peterkellner.net>
2015-05-19 17:12:54 -0700

    added 3pm tuesday back

--- angu/src/sessions/overview/controller.js
+++ angu/src/sessions/overview/controller.js
@@ -40,7 +40,7 @@ function SessionOverviewController(sessions, days, tracks, Speaker, times, $sce)
     var timesArray = [
         '8:00 AM', '8:30 AM',
         '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM',
-        '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM',
+        '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM','3:30 PM',
         '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM'
     ];
 
