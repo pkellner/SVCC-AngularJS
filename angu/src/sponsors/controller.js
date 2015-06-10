496a9301a4331f3a475153c110f06db1a3a951fa
pkellner <peter@peterkellner.net>
2015-06-09 18:35:28 -0700

    changed order

--- angu/src/sponsors/controller.js
+++ angu/src/sponsors/controller.js
@@ -3,6 +3,34 @@ export default function () {
 
   this.pluralsightAuthors = [
     {
+      "speakerId": 14842,
+      "authorName": "Dan Wahlin",
+      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20131216053752-14842.jpg",
+      "speakerUrl": "speaker/2015sf/dan-wahlin-14842",
+      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FDan%2DWahlin"
+    },
+    {
+      "speakerId": 31707,
+      "authorName": "John Papa",
+      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20150218195858-31707.jpg",
+      "speakerUrl": "speaker/2015sf/john-papa-31707",
+      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FJohn%2DPapa"
+    },
+    {
+      "speakerId": 1124,
+      "authorName": "Douglas Crockford",
+      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20131213212414-1124.jpg",
+      "speakerUrl": "speaker/2015sf/douglas-crockford-1124",
+      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FDouglas%2DCrockford"
+    },
+    {
+      "speakerId": 31682,
+      "authorName": "Misko Hevery",
+      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20150218182751-31682.jpg",
+      "speakerUrl": "speaker/2015sf/misko-hevery-31682",
+      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FMisko%2DHevery"
+    },
+    {
       "speakerId": 2000,
       "authorName": "Ward Bell",
       "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20150211122105-2000.jpg",
@@ -16,13 +44,7 @@ export default function () {
       "speakerUrl": "speaker/2015sf/andrew-connell-31688",
       "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FAndrew%2DConnell"
     },
-    {
-      "speakerId": 1124,
-      "authorName": "Douglas Crockford",
-      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20131213212414-1124.jpg",
-      "speakerUrl": "speaker/2015sf/douglas-crockford-1124",
-      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FDouglas%2DCrockford"
-    },
+
     {
       "speakerId": 31783,
       "authorName": "Joe Eames",
@@ -30,13 +52,7 @@ export default function () {
       "speakerUrl": "speaker/2015sf/joe-eames-31783",
       "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FJoe%2DEames"
     },
-    {
-      "speakerId": 31682,
-      "authorName": "Misko Hevery",
-      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20150218182751-31682.jpg",
-      "speakerUrl": "speaker/2015sf/misko-hevery-31682",
-      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FMisko%2DHevery"
-    },
+
     {
       "speakerId": 653,
       "authorName": "Deborah Kurata",
@@ -44,13 +60,7 @@ export default function () {
       "speakerUrl": "speaker/2015sf/deborah-kurata-653",
       "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FDeborah%2DKurata"
     },
-    {
-      "speakerId": 31707,
-      "authorName": "John Papa",
-      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20150218195858-31707.jpg",
-      "speakerUrl": "speaker/2015sf/john-papa-31707",
-      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FJohn%2DPapa"
-    },
+
     {
       "speakerId": 31685,
       "authorName": "Lukas Ruebbelke",
@@ -78,13 +88,7 @@ export default function () {
       "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20150306071301-31799.jpg",
       "speakerUrl": "speaker/2015sf/kyle-simpson-31799",
       "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FKyle%2DSimpson"
-    },
-    {
-      "speakerId": 14842,
-      "authorName": "Dan Wahlin",
-      "authorImageUrl": "//d1n4bbuvjcnilu.cloudfront.net/attendeeimage/20131216053752-14842.jpg",
-      "speakerUrl": "speaker/2015sf/dan-wahlin-14842",
-      "courseUrl": "https://www.shareasale.com/r.cfm?u=1015006&b=611266&m=53701&afftrack=&urllink=www%2Epluralsight%2Ecom%2Fauthor%2FDan%2DWahlin"
     }
+
   ];
 }
